19a9c63e0025f67cf5f6e56e3eccb114a8ba495e
Ayrton Souza <ayrtonsouza@icloud.com>
2020-06-24 19:19:06 -0300

    challenge completed

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,15 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transactionRepository.findOne(id);
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+    await transactionRepository.remove(transaction);
   }
 }
 
